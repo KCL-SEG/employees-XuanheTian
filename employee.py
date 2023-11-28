0fcb8d3fe80517b30cfd68b8d6312614faa1d3fc
Xuanhe Tian <xuanhe@txhsdell>
2023-11-28 15:59:39 +0000

    m

--- employee.py
+++ employee.py
@@ -68,3 +68,9 @@ class ContractCommission:
         return f"receives a commission for {self.number_of_contract} contract(s) at {self.contract_per_commission}/contract"
 
 
+billie = Employee('Billie', SalaryPay(4000))
+charlie = Employee('Charlie', HourlyPay(25, 100))
+renee = Employee('Renee', SalaryPay(3000), ContractCommission(4, 200))
+jan = Employee('Jan', HourlyPay(25, 150), ContractCommission(3, 220))
+robbie = Employee('Robbie', SalaryPay(2000), BonusCommission(1500))
+ariel = Employee('Ariel', HourlyPay(30, 120), BonusCommission(600))
